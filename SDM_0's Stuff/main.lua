1c51ffcd54b3b3417991136791b53d7784c77665
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-05-25 01:43:38 +0200

    Moved localization to its own file

--- SDM_0's Stuff/main.lua
+++ SDM_0's Stuff/main.lua
@@ -14,55 +14,7 @@
 
 SDM_0s_Stuff_Mod = SMODS.current_mod
 
-local parameters = NFS.load(SDM_0s_Stuff_Mod.path.."config.lua")()
-local space_jokers = parameters.space_jokers
-
-
-local sj_list= {}
-local temp_string = ""
-local words = 2
-local i = 0
-for _, v in pairs(space_jokers) do
-    if i < words then
-        temp_string = temp_string .. "{s:0.8}" .. v .. ", "
-        i = i + 1
-    end
-    if i >= words or next(space_jokers, _) == nil then
-        if next(space_jokers, _) == nil then
-            temp_string = temp_string:sub(1, -3)
-        end
-        table.insert(sj_list, temp_string)
-        temp_string = ""
-        i = 0
-    end
-end
-
-function SDM_0s_Stuff_Mod.process_loc_text()
-    G.localization.misc.dictionary.k_halved_ex = "Halved!"
-    G.localization.misc.dictionary.k_stone = "Stone"
-    G.localization.misc.dictionary.k_signed_ex = "Signed!"
-    G.localization.misc.dictionary.k_breached_ex = "Breached!"
-    G.localization.misc.dictionary.k_shared_ex = "Shared!"
-    G.localization.misc.v_dictionary.a_hand = "+#1# Hand"
-    G.localization.descriptions.Other.space_jokers = {
-        name = "Space Jokers",
-        text = sj_list
-    }
-    G.localization.descriptions.Other.chaos_exceptions = {
-        name = "Exceptions",
-        text = {
-            "Score round, score goal,",
-            "hand level and descriptions",
-        }
-    }
-    G.localization.descriptions.Other.perishable_no_debuff = {
-        name = "Perishable",
-        text = {
-            "Debuffed after",
-            "{C:attention}#1#{} rounds"
-        }
-    }
-end
+NFS.load(SDM_0s_Stuff_Mod.path.."localization.lua")()
 
 NFS.load(SDM_0s_Stuff_Mod.path.."utils.lua")()
 NFS.load(SDM_0s_Stuff_Mod.path.."overrides.lua")()
