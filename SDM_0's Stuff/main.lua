dbd4471079ff6644061ceb1350bc446b76293a1d
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-06-30 19:48:12 +0200

    Fixed add_to_deck context

--- SDM_0's Stuff/main.lua
+++ SDM_0's Stuff/main.lua
@@ -6,7 +6,7 @@
 --- BADGE_COLOUR: c20000
 --- DISPLAY_NAME: SDM_0's Stuff
 --- PREFIX: sdm
---- VERSION: 1.5.0
+--- VERSION: 1.5.0b
 --- LOADER_VERSION_GEQ: 1.0.0 
 
 ----------------------------------------------
--- SDM_0's Stuff/overrides.lua
+++ SDM_0's Stuff/overrides.lua
@@ -47,16 +47,4 @@ function Back:trigger_effect(args)
     end
 end
 
-local add_to_deckref = Card.add_to_deck
-function Card.add_to_deck(self, from_debuff)
-    if not self.added_to_deck then
-        if G.jokers and #G.jokers.cards > 0 then
-            for i = 1, #G.jokers.cards do
-                G.jokers.cards[i]:calculate_joker({sdm_adding_card = true, card = self})
-            end
-        end
-    end
-    add_to_deckref(self, from_debuff)
-end
-
 return
