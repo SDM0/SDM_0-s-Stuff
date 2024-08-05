68b6c81b9010256eae007df93c8be5bacd2a3558
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-08-06 00:37:38 +0200

    version bump

--- main.lua
+++ main.lua
@@ -6,8 +6,8 @@
 --- BADGE_COLOUR: c20000
 --- DISPLAY_NAME: SDM_0's Stuff
 --- PREFIX: sdm
---- VERSION: 1.6.0c
---- LOADER_VERSION_GEQ: 1.0.0 
+--- VERSION: 1.6.0d
+--- DEPENDENCIES: [Steamodded>=1.0.0-ALPHA]
 
 ----------------------------------------------
 ------------MOD CODE -------------------------
