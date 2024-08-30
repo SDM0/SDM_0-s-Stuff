c52266f48ed457d3bc4ea359c4da16b436486a29
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-08-30 16:53:06 +0200

    Moved text to its own localization folder

---
+++ localization/default.lua
@@ -0,0 +1,376 @@
+return {
+    descriptions = {
+        Joker = {
+            j_sdm_trance_the_devil = {
+                name = "Trance The Devil",
+                text = {
+                    "When {C:attention}Blind{} is selected,",
+                    "this Joker {C:attention}destroys{} each",
+                    "{C:attention}consumable{} for {X:red,C:white}X#1#{} Mult",
+                    "{C:inactive}(Currently {X:red,C:white}X#2#{C:inactive} Mult)" 
+                }
+            },
+            j_sdm_burger = {
+                name = "Burger",
+                text = {
+                    "{C:chips}+#3#{} Chips, {C:mult}+#2#{} Mult",
+                    "and {X:mult,C:white}X#1#{} Mult for",
+                    "the next {C:attention}#4#{} rounds",
+                }
+            },
+            j_sdm_bounciest_ball = {
+                name = "Bounciest Ball",
+                text = {
+                    "This Joker gains {C:chips}+#2#{} Chips when",
+                    "scoring most played {C:attention}poker hand{},",
+                    "halved on {C:attention}different hand{}",
+                    "{C:inactive}(Currently {C:attention}#3#{C:inactive}, {C:chips}+#1#{C:inactive} Chips)"
+                }
+            },
+            j_sdm_lucky_joker = {
+                name = "Lucky Joker",
+                text = {
+                    "Retrigger",
+                    "each played {C:attention}7{}",
+                    "{C:attention}#1#{} additional times"
+                }
+            },
+            j_sdm_iconic_icon = {
+                name = "Iconic Icon",
+                text = {
+                    "{C:mult}+#2#{} Mult per {C:attention}Ace{}",
+                    "in your {C:attention}full deck{},",
+                    "{C:mult}+#3#{} Mult if {C:attention}modified{}",
+                    "{C:inactive}(Currently {C:mult}+#1#{C:inactive} Mult)"
+                }
+            },
+            j_sdm_mult_n_chips = {
+                name = "Mult'N'Chips",
+                text = {
+                    "Scored {C:attention}Bonus{} cards",
+                    "give {C:mult}+#1#{} Mult and",
+                    "scored {C:attention}Mult{} cards",
+                    "give {C:chips}+#2#{} Chips",
+                }
+            },
+            j_sdm_moon_base = {
+                name = "Moon Base",
+                text = {
+                    "When {C:attention}Blind{} is selected,",
+                    "create a random",
+                    "{C:attention}Space{} Joker",
+                    "{s:0.8,C:inactive}(Cannot create Moon Base)",
+                    "{C:inactive}(Must have room)"
+                }
+            },
+            j_sdm_shareholder_joker = {
+                name = "Shareholder Joker",
+                text = {
+                    "Earn between {C:money}$#1#{} and {C:money}$#2#{}",
+                    "at end of round",
+                }
+            },
+            j_sdm_magic_hands = {
+                name = "Magic Hands",
+                text = {
+                    "{X:mult,C:white}X#1#{} Mult if scored {C:attention}poker hand{} has",
+                    "exactly {C:blue}#2#{} {C:inactive,s:0.8}(= hands left before Play){}",
+                    "of its most frequent rank",
+                    "{C:inactive}(ex: {C:attention}K K K Q Q{C:inactive} with {C:blue}3{C:inactive} hands left)",
+                }
+            },
+            j_sdm_tip_jar = {
+                name = "Tip Jar",
+                text = {
+                    "Earn your money's",
+                    "{C:attention}highest digit",
+                    "at end of round",
+                    "{C:inactive}(ex: {C:money}$24{C:inactive} -> {C:money}$4{C:inactive})",
+                }
+            },
+            j_sdm_wandering_star = {
+                name = "Wandering Star",
+                text = {
+                    "Creates a copy of",
+                    "a used {C:planet}Planet{} card,",
+                    "once per {C:attention}round{}",
+                    "{s:0.8,C:inactive}(Must have room)",
+                    "{C:inactive}(Currently {C:attention}#1#{C:inactive}#2#)"
+                }
+            },
+            j_sdm_ouija_board = {
+                name = "Ouija Board",
+                text = {
+                    "After selling a {C:red}Rare {C:attention}Joker{},",
+                    "scoring a {C:attention}secret poker hand{}",
+                    "and using a {C:spectral}Spectral{} card,",
+                    "sell this Joker to create a {C:spectral}Soul{} card",
+                    "{s:0.8,C:inactive}(Must have room)",
+                    "{C:inactive}(Remaining {C:attention}#3#{C:inactive}#4#/{C:attention}#5#{C:inactive}#6#/{C:attention}#7#{C:inactive}#8#)"
+                }
+            },
+            j_sdm_la_revolution = {
+                name = "La Révolution",
+                text = {
+                    "Upgrade {C:attention}winning hand{}",
+                    "played without {C:attention}face{} cards",
+                }
+            },
+            j_sdm_clown_bank = {
+                name = "Clown Bank",
+                text = {
+                    "When {C:attention}Blind{} is selected, spend {C:money}$#3#{}",
+                    "to give this Joker {X:mult,C:white}X#2#{} Mult",
+                    "and increase requirement by {C:money}$#4#{}",
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{C:inactive} Mult)"
+                }
+            },
+            j_sdm_furnace = {
+                name = "Furnace",
+                text = {
+                    "When round begins,",
+                    "add a random {C:attention}Gold{} or",
+                    "{C:attention}Steel{} playing card",
+                }
+            },
+            j_sdm_warehouse = {
+                name = "Warehouse",
+                text = {
+                    "{C:attention}+#1#{} hand size,",
+                    "{C:red}-#2#{} consumable slot,",
+                    "lose {C:money}$#3#{} if sold"
+                }
+            },
+            j_sdm_zombie_joker = {
+                name = "Zombie Joker",
+                text = {
+                    "{C:green}#1# in #2#{} chance to create a",
+                    "{C:tarot}Death{} card when {C:attention}selling{}",
+                    "a card other than {C:tarot}Death{}",
+                    "{C:inactive}(Must have room)"
+                }
+            },
+            j_sdm_mystery_joker = {
+                name = "Mystery Joker",
+                text = {
+                    "Create a {C:red}Rare {C:attention}Tag{} when",
+                    "{C:attention}Boss Blind{} is defeated",
+                }
+            },
+            j_sdm_infinite_staircase = {
+                name = "Infinite Staircase",
+                text = {
+                    "{X:mult,C:white}X#1#{} Mult if scored hand",
+                    "contains a {C:attention}numerical{}",
+                    "{C:attention}Straight{} without an {C:attention}Ace{}",
+                }
+            },
+            j_sdm_ninja_joker = {
+                name = "Ninja Joker",
+                text = {
+                    "Creates a {C:dark_edition}Negative{C:attention} Tag{} if",
+                    "a playing card is {C:attention}destroyed{},",
+                    "becomes inactive until a",
+                    "{C:attention}playing card{} is added",
+                    "{C:inactive}(Currently {C:attention}#1#{C:inactive}#2#{C:inactive})"
+                }
+            },
+            j_sdm_reach_the_stars = {
+                name = "Reach The Stars",
+                text = {
+                    "Scoring {C:attention}#1#{} and {C:attention}#2#{} cards",
+                    "creates a random {C:planet}Planet{} card",
+                    "{s:0.8}Changes at end of round",
+                    "{C:inactive}(Must have room)",
+                    "{C:inactive}(Currently {C:attention}#3#{C:inactive}#4# & {C:attention}#5#{C:inactive}#6#)"
+                }
+            },
+            j_sdm_crooked_joker = {
+                name = "Crooked Joker",
+                text = {
+                    "{C:attention}Doubles{} or {C:red}destroys{}",
+                    "each added {C:attention}Joker{}",
+                    "{C:inactive}(Must have room)"
+                }
+            },
+            j_sdm_property_damage = {
+                name = "Property Damage",
+                text = {
+                    "This Joker gains {X:mult,C:white}X#2#{} Mult",
+                    "when discarded hand",
+                    "contains a {C:attention}Full House{}",
+                    "{C:inactive}(Currently {X:mult,C:white}X#1#{} {C:inactive}Mult)",
+                }
+            },
+            j_sdm_rock_n_roll = {
+                name = "Rock'N'Roll",
+                text = {
+                    "Retrigger all played",
+                    "{C:attention}enhanced{} cards",
+                }
+            },
+            j_sdm_contract = {
+                name = "Contract",
+                text = {
+                    "Earn {C:money}$#1#{} at",
+                    "end of round,",
+                    "{C:red}#2#X{} Blind goal",
+                }
+            },
+            j_sdm_cupidon = {
+                name = "Cupidon",
+                text = {
+                    "{X:attention,C:white}X#1#{} Blind",
+                    "requirement",
+                }
+            },
+            j_sdm_pizza = {
+                name = "Pizza",
+                text = {
+                    "When {C:attention}Blind{} is selected,",
+                    "gain {C:blue}+#1#{} #3#",
+                    "{C:blue}-#2#{} per round played"
+                }
+            },
+            j_sdm_treasure_chest = {
+                name = "Treasure Chest",
+                text = {
+                    "Gains {C:money}$#1#{} of {C:attention}sell value{}",
+                    "per {C:attention}consumable{} sold",
+                }
+            },
+            j_sdm_bullet_train = {
+                name = "Bullet Train",
+                text = {
+                    "{C:chips}+#1#{} Chips on your",
+                    "{C:attention}first hand{} if no discards",
+                    "were used this round",
+                }
+            },
+            j_sdm_chaos_theory = {
+                name = "Chaos Theory",
+                text = {
+                    "Adds {C:attention}double{} the value of all",
+                    "{C:attention}on-screen numbers{} to Chips",
+                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
+                }
+            },
+            j_sdm_jambo = {
+                name = "Jambo",
+                text = {
+                    "Adds {C:attention}double{}",
+                    "the rank of a",
+                    "random {C:attention}discarded{}",
+                    "card to Mult",
+                }
+            },
+            j_sdm_water_slide = {
+                name = "Water Slide",
+                text = {
+                    "This Joker gains {C:chips}+#1#{}",
+                    "Chips if scored hand",
+                    "contains a {C:attention}9{}, {C:attention}7{} or {C:attention}6",
+                    "{C:inactive}(Currently {C:chips}+#2#{C:inactive} Chips)"
+                }
+            },
+            j_sdm_joker_voucher = {
+                name = "Joker Voucher",
+                text = {
+                    "{X:mult,C:white}X#1#{} Mult per redeemed",
+                    "{C:attention}Voucher{} this run",
+                    "{C:inactive}(Currently {X:mult,C:white}X#2#{C:inactive} Mult)"
+                }
+            },
+            j_sdm_free_pass = {
+                name = "Free Pass",
+                text = {
+                    "If {C:attention}first hand{} or {C:attention}discard{}",
+                    "of round has only {C:attention}1{} card,",
+                    "gain {C:attention}+#1#{} {C:blue}hand{} or {C:red}discard{}",
+                    "respectively",
+                }
+            },
+            j_sdm_legionary_joker = {
+                name = "Legionary Joker",
+                text = {
+                    "Each {C:spades}Spade{} or {C:diamonds}Diamond{}",
+                    "card held in hand",
+                    "gives {C:mult}+#1#{} Mult",
+                },
+            },
+            j_sdm_archibald = {
+                name = "Archibald",
+                text = {
+                    "After {C:attention}Boss Blind{}, creates",
+                    "a {C:dark_edition}Negative{} copy of a",
+                    "random {C:attention}Joker{} at the",
+                    "end of the {C:attention}shop",
+                    "{s:0.8,C:inactive}(Cannot copy Archibald)",
+                    "{C:inactive}(Currently {C:attention}#1#{C:inactive}#2#{C:inactive})"
+                }
+            },
+            j_sdm_0 = { -- Steamodded thinks "sdm" overwrites the prefix
+                name = "SDM_0",
+                text = {
+                    "This Joker gains {C:dark_edition}+#1#{} Joker",
+                    "Slots per destroyed {C:attention}2{}s",
+                    "{C:inactive}(Currently {C:dark_edition}+#2# {C:inactive}Joker #3#)"
+                }
+            },
+        },
+        Other = {
+            space_jokers = {
+                name = "Space Jokers",
+                text = {
+                    "Astronomer, Constellation,",
+                    "Rocket, Satellite,",
+                    "Space Joker, Supernova etc.",
+                }
+            },
+            modified_card = {
+                name = "Modified",
+                text = {
+                    "Enhancement, seal,",
+                    "edition"
+                }
+            },
+            chaos_exceptions = {
+                name = "Exceptions",
+                text = {
+                    "Round score, goal score,",
+                    "hand level, and descriptions",
+                }
+            },
+            perishable_no_debuff = {
+                name = "Perishable",
+                text = {
+                    "Debuffed after",
+                    "{C:attention}#1#{} rounds"
+                }
+            },
+            resources = {
+                name = "Resources",
+                text = {
+                    "Hand, discard, dollar, handsize,",
+                    "joker slot, consumable slot"
+                }
+            }
+        }
+    },
+    misc = {
+        dictionary = {
+            k_halved_ex = "Halved!",
+            k_shared_ex = "Shared!",
+        },
+        v_dictionary = {
+            a_hand = "+#1# Hand",
+            a_hand_minus = "-#1# Hand",
+            a_discard = "+#1# Discard",
+            a_discard_minus = "-#1# Discard",
+            a_joker_slot = "+#1# Joker Slot",
+            a_joker_slot_minus = "-#1# Joker Slot",
+            a_consumable_slot = "+#1# Cons. Slot",
+            a_consumable_slot_minus = "-#1# Cons. Slot",
+        }
+    },
+}
