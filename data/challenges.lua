a9efbae8854cd1a68c4b4cf8a00aeea9920ee61f
SDM0 <60699143+SDM0@users.noreply.github.com>
2024-09-10 18:48:43 +0200

    Revert "Revamped config handlers"
    This reverts commit 9abaafbe1693ceeee16984cafa860f55c69d5f6c.

--- data/challenges.lua
+++ data/challenges.lua
@@ -1,142 +1,146 @@
 --- Challenges ---
 
---- Scientific Downfall ---
+if sdm_config.sdm_jokers then
 
-SMODS.Challenge{
-    loc_txt = "Scientific Downfall",
-    key = 'scientific_downfall',
-    rules = {
-        custom = {
-            {id = 'no_shop_planets'},
+    --- Scientific Downfall ---
+
+    SMODS.Challenge{
+        loc_txt = "Scientific Downfall",
+        key = 'scientific_downfall',
+        rules = {
+            custom = {
+                {id = 'no_shop_planets'},
+            },
+            modifiers = {
+                {id = 'discards', value = 4},
+                {id = 'hands', value = 2},
+            },
         },
-        modifiers = {
-            {id = 'discards', value = 4},
-            {id = 'hands', value = 2},
+        jokers = {
+            {id = 'j_sdm_la_revolution', eternal = true},
         },
-    },
-    jokers = {
-        {id = 'j_sdm_la_revolution', eternal = true},
-    },
-    restrictions = {
-        banned_cards = {
-            {id = 'c_high_priestess'},
-            {id = 'c_black_hole'},
-            {id = 'c_trance'},
-            {id = 'p_celestial_normal_1', ids = {
-                'p_celestial_normal_1','p_celestial_normal_2','p_celestial_normal_3','p_celestial_normal_4','p_celestial_jumbo_1','p_celestial_jumbo_2','p_celestial_mega_1','p_celestial_mega_2',
-            }},
-            {id = 'j_8_ball'},
-            {id = 'j_space'},
-            {id = 'j_constellation'},
-            {id = 'j_certificate'},
-            {id = 'j_satellite'},
-            {id = 'j_astronomer'},
-            {id = 'j_burnt'},
-            {id = 'j_sdm_wandering_star'},
-            {id = 'v_telescope'},
-            {id = 'v_observatory'},
-            {id = 'v_planet_merchant'},
-            {id = 'v_planet_tycoon'},
-        },
-        banned_tags = {
-            {id = 'tag_meteor'},
-            {id = 'tag_orbital'},
-        },
-        banned_other = {},
+        restrictions = {
+            banned_cards = {
+                {id = 'c_high_priestess'},
+                {id = 'c_black_hole'},
+                {id = 'c_trance'},
+                {id = 'p_celestial_normal_1', ids = {
+                    'p_celestial_normal_1','p_celestial_normal_2','p_celestial_normal_3','p_celestial_normal_4','p_celestial_jumbo_1','p_celestial_jumbo_2','p_celestial_mega_1','p_celestial_mega_2',
+                }},
+                {id = 'j_8_ball'},
+                {id = 'j_space'},
+                {id = 'j_constellation'},
+                {id = 'j_certificate'},
+                {id = 'j_satellite'},
+                {id = 'j_astronomer'},
+                {id = 'j_burnt'},
+                {id = 'j_sdm_wandering_star'},
+                {id = 'v_telescope'},
+                {id = 'v_observatory'},
+                {id = 'v_planet_merchant'},
+                {id = 'v_planet_tycoon'},
+            },
+            banned_tags = {
+                {id = 'tag_meteor'},
+                {id = 'tag_orbital'},
+            },
+            banned_other = {},
+        }
     }
-}
 
---- A Plumber's Hassle ---
+    --- A Plumber's Hassle ---
 
-SMODS.Challenge{
-    loc_txt = "A Plumber's Hassle",
-    key = 'a_plumber_s_hassle',
-    rules = {
-        custom = {},
-        modifiers = {
-            {id = 'joker_slots', value = 4},
-            {id = 'hands', value = 3},
+    SMODS.Challenge{
+        loc_txt = "A Plumber's Hassle",
+        key = 'a_plumber_s_hassle',
+        rules = {
+            custom = {},
+            modifiers = {
+                {id = 'joker_slots', value = 4},
+                {id = 'hands', value = 3},
+            },
         },
-    },
-    jokers = {
-        {id = 'j_sdm_infinite_staircase', eternal = true},
-        {id = 'j_runner', eternal = true},
-    },
-    restrictions = {
-        banned_cards = {
-            {id = 'v_grabber'},
-            {id = 'v_nacho_tong'},
-            {id = 'j_burglar'},
-            {id = 'j_sdm_pizza'},
+        jokers = {
+            {id = 'j_sdm_infinite_staircase', eternal = true},
+            {id = 'j_runner', eternal = true},
         },
-        banned_tags = {},
-        banned_other = {}
+        restrictions = {
+            banned_cards = {
+                {id = 'v_grabber'},
+                {id = 'v_nacho_tong'},
+                {id = 'j_burglar'},
+                {id = 'j_sdm_pizza'},
+            },
+            banned_tags = {},
+            banned_other = {}
+        }
     }
-}
 
---- Spare Change ---
+    --- Spare Change ---
 
-SMODS.Challenge{
-    loc_txt = "Spare Change",
-    key = 'spare_change',
-    rules = {
-        custom = {
-            {id = 'no_extra_hand_money'},
-            {id = 'no_interest'}
+    SMODS.Challenge{
+        loc_txt = "Spare Change",
+        key = 'spare_change',
+        rules = {
+            custom = {
+                {id = 'no_extra_hand_money'},
+                {id = 'no_interest'}
+            },
+            modifiers = {},
         },
-        modifiers = {},
-    },
-    jokers = {
-        {id = 'j_sdm_clown_bank', eternal = true},
-        {id = 'j_sdm_tip_jar', eternal = true},
-    },
-    restrictions = {
-        banned_cards = {
-            {id = 'v_seed_money'},
-            {id = 'v_money_tree'},
-            {id = 'j_to_the_moon'},
-            {id = 'j_rocket'},
-            {id = 'j_golden'},
-            {id = 'j_satellite'},
-            {id = 'j_sdm_shareholder_joker'},
+        jokers = {
+            {id = 'j_sdm_clown_bank', eternal = true},
+            {id = 'j_sdm_tip_jar', eternal = true},
         },
-        banned_tags = {},
-        banned_other = {}
+        restrictions = {
+            banned_cards = {
+                {id = 'v_seed_money'},
+                {id = 'v_money_tree'},
+                {id = 'j_to_the_moon'},
+                {id = 'j_rocket'},
+                {id = 'j_golden'},
+                {id = 'j_satellite'},
+                {id = 'j_sdm_shareholder_joker'},
+            },
+            banned_tags = {},
+            banned_other = {}
+        }
     }
-}
 
---- Twisted Binding ---
+    --- Twisted Binding ---
 
-SMODS.Challenge{
-    loc_txt = "Twisted Binding",
-    key = 'twisted_binding',
-    jokers = {
-        {id = 'j_sdm_crooked_joker', eternal = true},
-    },
-}
-
---- Archifoolery ---
-
-SMODS.Challenge{
-    loc_txt = "Archifoolery",
-    key = 'Archifoolery',
-    rules = {
-        custom = {},
-        modifiers = {
-            {id = 'joker_slots', value = 1},
+    SMODS.Challenge{
+        loc_txt = "Twisted Binding",
+        key = 'twisted_binding',
+        jokers = {
+            {id = 'j_sdm_crooked_joker', eternal = true},
         },
-    },
-    jokers = {
-        {id = 'j_sdm_archibald', edition = 'negative', eternal = true},
-    },
-    restrictions = {
-        banned_cards = {
-            {id = 'v_blank'},
-            {id = 'v_antimatter'},
-        },
-        banned_tags = {},
-        banned_other = {}
     }
-}
+
+    --- Archifoolery ---
+
+    SMODS.Challenge{
+        loc_txt = "Archifoolery",
+        key = 'Archifoolery',
+        rules = {
+            custom = {},
+            modifiers = {
+                {id = 'joker_slots', value = 1},
+            },
+        },
+        jokers = {
+            {id = 'j_sdm_archibald', edition = 'negative', eternal = true},
+        },
+        restrictions = {
+            banned_cards = {
+                {id = 'v_blank'},
+                {id = 'v_antimatter'},
+            },
+            banned_tags = {},
+            banned_other = {}
+        }
+    }
+
+end
 
 return
