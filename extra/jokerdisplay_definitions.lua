529ce7d110922ef01518535772eeaa276c25583b
SDM0 <60699143+SDM0@users.noreply.github.com>
2025-02-28 22:37:12 +0100

    Fixed "Skelton" incorrect JD format

--- extra/jokerdisplay_definitions.lua
+++ extra/jokerdisplay_definitions.lua
@@ -505,12 +505,17 @@ jd_def["j_sdm_0"] = { -- SDM_0
     },
 }
 jd_def["j_sdm_skelton"] = { -- Skelton
+text = {
+    { text = "+$" },
+    { ref_table = "card.ability.extra", ref_value = "dollars" },
+    },
+    text_config = { colour = G.C.GOLD },
     reminder_text = {
-        { text = "(" },
-        { text = "$",                       colour = G.C.GOLD },
-        { ref_table = "card.ability.extra", ref_value = "dollars", colour = G.C.GOLD },
-        { text = ")" },
-    }
+        { ref_table = "card.joker_display_values", ref_value = "localized_text" },
+    },
+    calc_function = function(card)
+        card.joker_display_values.localized_text = "(" .. localize("k_round") .. ")"
+    end
 }
 jd_def["j_sdm_trance_the_devil"] = { -- Trance The Devil
     text = {
